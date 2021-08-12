402e0bbd400fb9e5b80e8d2ce5710f1e54115f47
xiaochenll <hxiaochenll@gmail.com>
2021-08-12 17:57:10 +0800

    Create x86.sh

---
+++ x86.sh
@@ -0,0 +1,4 @@
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+git clone https://github.com/fw876/helloworld package/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/immortalwrt/openwrt-filebrowser package/openwrt-filebrowser
