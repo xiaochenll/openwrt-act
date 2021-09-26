917737e08d55a457d32c67b6fe2aabc912b54c85
xiaochenll <hxiaochenll@gmail.com>
2021-09-26 08:10:46 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,4 +1,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 git clone https://github.com/fw876/helloworld package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-git clone https://github.com/immortalwrt/openwrt-filebrowser package/openwrt-filebrowser
+#git clone https://github.com/immortalwrt/openwrt-filebrowser package/openwrt-filebrowser
+git clone https://github.com/xiaochenll/openwrt-filebrowser package/openwrt-filebrowser
