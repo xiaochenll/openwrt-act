5ee6606f1d6f5fa50011385f08e40629e6430569
xiaochenll <hxiaochenll@gmail.com>
2021-09-29 17:49:42 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -2,3 +2,4 @@
 git clone https://github.com/fw876/helloworld package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone https://github.com/immortalwrt/openwrt-filebrowser package/openwrt-filebrowser
+git clone https://github.com/immortalwrt/openwrt-gowebdav package/openwrt-gowebdav
