9fe1937ab9d446a395b4d5aa27fdd2d4fef5d4e8
xiaochenll <hxiaochenll@gmail.com>
2022-02-24 16:19:41 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,3 +1,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 git clone https://github.com/fw876/helloworld package/helloworld
-git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+git clone -b 18.06 https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
