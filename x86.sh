ac74e0af5a3385988dd8c7811dd98dd0664f6da7
xiaochenll <hxiaochenll@gmail.com>
2022-02-26 15:58:54 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,3 +1,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 git clone https://github.com/fw876/helloworld package/helloworld
 git clone https://github.com/immortalwrt-collections/openwrt-filebrowser package/openwrt-filebrowser
+git clone https://github.com/immortalwrt-collections/openwrt-gowebdav package/openwrt-gowebdav
