d7a0e5fd9aa5e2a721213cbdd606044d5ea7e089
xiaochenll <hxiaochenll@gmail.com>
2022-04-15 10:08:51 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,7 +1,5 @@
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-git clone https://github.com/fw876/helloworld package/helloworld
-git clone https://github.com/immortalwrt-collections/openwrt-filebrowser package/openwrt-filebrowser
-git clone https://github.com/immortalwrt-collections/openwrt-gowebdav package/openwrt-gowebdav
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
-git clone https://github.com/sundaqiang/openwrt-packages package/openwrt-packages
-echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git openwrt-filebrowser https://github.com/immortalwrt-collections/openwrt-filebrowser' >>feeds.conf.default
+echo 'src-git openwrt-passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
