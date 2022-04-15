b8ee1970cfdbfce1c3f1aab3f5b874e8363ee41e
xiaochenll <hxiaochenll@gmail.com>
2022-04-15 10:24:12 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,5 +1,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git openwrt-passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/immortalwrt-collections/openwrt-filebrowser package/openwrt-filebrowser 
+git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
