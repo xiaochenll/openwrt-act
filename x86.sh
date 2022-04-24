dd58c75b9fa8ae0a7991747185b01b319483f3f7
xiaochenll <hxiaochenll@gmail.com>
2022-04-24 20:40:22 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -4,3 +4,4 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/immortalwrt-collections/openwrt-filebrowser package/openwrt-filebrowser 
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
+sed -i 's/Os/O3 -funsafe-math-optimizations -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections/g' include/target.mk
