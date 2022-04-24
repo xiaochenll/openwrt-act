c239ca182c28d0d61d58139159c28954e7bd4bf8
xiaochenll <hxiaochenll@gmail.com>
2022-04-24 20:41:36 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,7 +1,8 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/immortalwrt-collections/openwrt-filebrowser package/openwrt-filebrowser 
+git clone https://github.com/sbwml/openwrt-filebrowser package/openwrt-filebrowser
+git clone https://github.com/sbwml/openwrt-alist package/openwrt-alist
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
 sed -i 's/Os/O3 -funsafe-math-optimizations -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections/g' include/target.mk
