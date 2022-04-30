d0f6f3a23a65e3961a1033adbc98156eb897b9e2
xiaochenll <hxiaochenll@gmail.com>
2022-04-30 20:40:26 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -4,3 +4,6 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/xiaochenll/openwrt-filebrowser package/openwrt-filebrowser
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
 git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
+git clone https://github.com/koshev-msk/luci-app-3proxy  package/luci-app-3proxy
+git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/openwrt-cdnspeedtest
+git clone https://github.com/xiaochenll/3proxy-openwrt feeds/packages/net/3proxy
