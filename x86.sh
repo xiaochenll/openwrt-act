fa5bbc3d71b14de23c1a6bc531d761bc886c00df
xiaochenll <hxiaochenll@gmail.com>
2022-05-06 16:39:17 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -4,6 +4,3 @@ echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/openwrt-cdnspeedtest
 git clone https://github.com/xiaochenll/openwrt-filebrowser package/openwrt-filebrowser
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/openwrt-passwall2
-git clone https://github.com/kiddin9/openwrt-bypass package/openwrt-bypass
-git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
