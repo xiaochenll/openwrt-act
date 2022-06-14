d6a03ca165efdaf4d0978b980e26b920d3500356
xiaochenll <hxiaochenll@gmail.com>
2022-06-14 11:22:34 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -3,3 +3,4 @@ echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >>feeds
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/openwrt-cdnspeedtest
 git clone https://github.com/xiaochenll/openwrt-filebrowser package/openwrt-filebrowser
+git clone https://github.com/vernesong/OpenClash package/OpenClash
