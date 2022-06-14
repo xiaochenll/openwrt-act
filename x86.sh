1d20660f129313ca9cc8da214d9e83790ed35405
xiaochenll <hxiaochenll@gmail.com>
2022-06-14 11:37:04 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -3,4 +3,11 @@ echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >>feeds
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/openwrt-cdnspeedtest
 git clone https://github.com/xiaochenll/openwrt-filebrowser package/openwrt-filebrowser
-git clone https://github.com/vernesong/OpenClash package/OpenClash
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
