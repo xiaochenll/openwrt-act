f0b6e056c1556d875222e31a540d2b6719ced35a
xiaochenll <hxiaochenll@gmail.com>
2022-06-15 10:07:14 +0800

    Update x86.sh

--- x86.sh
+++ x86.sh
@@ -1,6 +1,7 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/openwrt-cdnspeedtest
 git clone https://github.com/xiaochenll/openwrt-filebrowser package/openwrt-filebrowser
 git clone https://github.com/xiaochenll/openwrt-adguardhome package/openwrt-adguardhome
